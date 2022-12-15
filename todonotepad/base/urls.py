cae6358cb5fd7b279284b236579220d61aa41a91
emmanuelatikese <emmanuelatikese14@gmail.com>
2022-12-15 08:09:54 -0800

    add

--- todonotepad/base/urls.py
+++ todonotepad/base/urls.py
@@ -10,13 +10,13 @@ urlpatterns = [
     path('todolist/', Todolist.as_view(), name='todotasks'),
     path('tododelete/<str:pk>/', TodoDelete.as_view(), name='Tododelete'),
     path('todoCreate/', TodoCreate.as_view(), name='todoCreate'),
-    path('todoUpdate/', TodoUpdate.as_view(), name='todoUpdate'),
+    path('todoUpdate/<str:pk>/', TodoUpdate.as_view(), name='todoUpdate'),
     path('MainRegister/', MainRegister.as_view(), name='MainRegister'),
     path('MainLogin/', MainLogin.as_view(), name='MainLogin'),
     path('LogoutView', LogoutView.as_view(next_page='MainLogin'), name='Logout'),
     # Now the paths for the notepad
     path('notepadHome/', views.notepadHome, name='notepadHome'),
     path('notepadRoom/', views.notepadRoom, name='notepadRoom'),
-    path('notepadCreate/',views.notepadCreate, name="notepadCreate"),
-    path('notepadUpdate/', views.notepadUpdate, name="notepadUpdate"),
+    path('notepadCreate/<str:pk>/',views.notepadCreate, name="notepadCreate"),
+    path('notepadUpdate/<str:pk>/', views.notepadUpdate, name="notepadUpdate"),
 ]
